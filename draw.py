52de300dee32322283baca31457f9c4b8cc05b25
Karen Li <kli19@stuy.edu>
2019-03-16 20:54:33 -0400

    add box

--- draw.py
+++ draw.py
@@ -7,7 +7,18 @@ from matrix import *
   # height and depth dimensions.
   # ====================
 def add_box( points, x, y, z, width, height, depth ):
-    pass
+    add_edge(x, y, z, x + width, y, z)
+    add_edge(x + width, y, z, x + width, y - height, z)
+    add_edge(x + width, y - height, z, x, y-height, z)
+    add_edge(x, y-height, z, x, y, z)
+    add_edge(x, y, z - depth, x + width, y, z - depth)
+    add_edge(x + width, y, z - depth, x + width, y - height, z - depth)
+    add_edge(x + width, y - height, z - depth, x, y-height, z - depth)
+    add_edge(x, y-height, z - depth, x, y, z - depth)
+    add_edge(x, y, z, x, y, z - depth)
+    add_edge(x + width, y, z, x + width, y, z - depth)
+    add_edge(x + width, y - height, z, x + width, y - height, z - depth)
+    add_edge(x, y-height, z, x, y - height, z - depth)
 
   # ====================
   # Generates all the points along the surface

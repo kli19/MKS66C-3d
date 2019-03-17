e0f1255065eb840a5c313921b0dea838c12a0904
Karen Li <kli19@stuy.edu>
2019-03-16 21:19:44 -0400

    parse torus'

--- parser.py
+++ parser.py
@@ -76,6 +76,10 @@ def parse_file( fname, edges, transform, screen, color ):
                        float(args[0]), float(args[1]), float(args[2]),
                        float(args[3]), 20)
 
+        elif line == 'torus':
+            add_torus(edges, float(args[0]), float(args[1]),
+                      float(args[2]), float(args[3]), float(args[4]), 20)
+
         elif line == 'hermite' or line == 'bezier':
             #print 'curve\t' + line + ": " + str(args)
             add_curve(edges,
